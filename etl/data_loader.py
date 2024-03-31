b8aa28c51c978196effb4251eb70dd628fa99dce
v.volkovskiy <v.volkovskiy@yadro.com>
2024-03-31 13:23:39 +0300

    draft commit 3

--- etl/data_loader.py
+++ etl/data_loader.py
@@ -1,11 +1,12 @@
 import datetime
 import json
+import logging
 import os
+import sys
 import time
 from contextlib import closing
 from dataclasses import dataclass
 from functools import wraps
-from pprint import pprint
 from typing import Any
 from uuid import UUID
 
@@ -13,9 +14,9 @@ import psycopg2
 import pydantic
 import requests
 from dotenv import load_dotenv
-from psycopg2 import InterfaceError, OperationalError
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor
+from redis import Redis
 
 load_dotenv()
 
@@ -27,36 +28,38 @@ dsl = {
     'port': os.environ.get('DB_PORT'),
 }
 
+redis_connection = {
+    'host': os.environ.get('REDIS_HOST'),
+    'port': os.environ.get('REDIS_PORT'),
+}
+
+# update_period = 21600  # каждые 6 часов
+update_period = 30
+
+
 @dataclass
 class Film:
-    # class Film(pydantic.BaseModel):
     fw_id: UUID
     title: str
     rating: float
     type: str
     created_at: datetime.datetime
     updated_at: datetime.datetime
-    writers_names: list[str]
-    directors_names: list[str]
-    actors_names: list[str]
     writers: list[dict]
     actors: list[dict]
     directors: list[dict]
     genres: list[str]
+    writers_names: list[str] = pydantic.Field(default=[])
+    directors_names: list[str] = pydantic.Field(default=[])
+    actors_names: list[str] = pydantic.Field(default=[])
     description: str | None = pydantic.Field(default='Description in progress...')
 
 
-# TODO настроить респонс из эластика с фильтрами по ошибкам - OK
-# TODO загрузка по жанрам - ОК
-# TODO загрузка по фильмам - ОК
-# TODO backoff - эластик ОК, сделать для постгри
-# TODO backoff - передавать в декоратор список эксепшенов для каждого метода (че с ним делать дальше?)
-# TODO backoff - считать среднее кол-во обратываемых запросов в день для мониторинга работы ETL, уведомлять при большой дельте от среднего числа
-# TODO redis (хранить состояние) -
+# TODO считать среднее кол-во обратываемых запросов в день для мониторинга работы ETL, уведомлять при большой дельте от среднего числа  # noqa E501
 # TODO упаковка в докер
 
 
-# TODO: добавить jitter в бэкофф
+# # TODO: добавить jitter в бэкофф
 def backoff(exceptions: tuple, start_sleep_time=0.1, factor=2, border_sleep_time=100):
     """
     Функция для повторного выполнения функции через некоторое время, если возникла ошибка.
@@ -75,24 +78,31 @@ def backoff(exceptions: tuple, start_sleep_time=0.1, factor=2, border_sleep_time
         @wraps(func)
         def inner(*args, **kwargs):
             delay = start_sleep_time * factor
+            logger = args[0].logger
             while delay < border_sleep_time:
                 try:
                     return func(*args, **kwargs)
-                except exceptions as e:
-                    print(f'waiting {delay} seconds before retry')
+                except exceptions:
+                    logger.info(f'Ждем {delay} секунд для повторения запроса')
                     time.sleep(delay)
                     delay = delay * factor
             return func(*args, **kwargs)
+
         return inner
     return func_wrapper
 
 
-# TODO  протестить реконнекты к базе, определить откуда продолжается выгрузка данных из БД
 def db_reconnect(start_sleep_time=0.1, factor=2, border_sleep_time=100):
+    """
+    Функция для проверки актуальности подключения к БД.
+    В случае разрыва соединения, создается новое подключюние к БД.
+    """
+
     def func_wrapper(func):
         @wraps(func)
         def inner(*args, **kwargs):
             delay = start_sleep_time * factor
+            logger = args[0].logger
             if args[0].pg_conn.closed != 0:
                 while delay < border_sleep_time:
                     try:
@@ -100,7 +110,7 @@ def db_reconnect(start_sleep_time=0.1, factor=2, border_sleep_time=100):
                         args[0].pg_conn = pg_conn
                         return func(*args, **kwargs)
                     except psycopg2.Error:
-                        print(f'waiting {delay} seconds before retry')
+                        logger.info(f'Ждем {delay} секунд для повторения запроса')
                         time.sleep(delay)
                         delay = delay * factor
             return func(*args, **kwargs)
@@ -108,22 +118,32 @@ def db_reconnect(start_sleep_time=0.1, factor=2, border_sleep_time=100):
     return func_wrapper
 
 
-class ProtoUpdater:
-    default_last_update_time = '2021-06-16 23:00:00.000 +0300'
-    persons_last_update: str = default_last_update_time
-    genres_last_update: str = default_last_update_time
-    films_last_update: str = default_last_update_time
-    already_updated_films = []
-    pg_conn: _connection
+class CustomLogger(logging.Logger):
+    def __init__(self, name: str):
+        super().__init__(name)
+        formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s", "%Y-%m-%d %H:%M:%S")
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(formatter)
+        self.addHandler(console_handler)
+        self.setLevel(level='INFO')
 
-    def __init__(self, pg_conn: _connection):
+
+class ProtoUpdater:
+    """Родительский класс для объединения общих методов и аттрибутов дочерних классов."""
+    already_updated_films: list = []
+    pg_conn: _connection
+    redis: Redis
+
+    def __init__(self, pg_conn: _connection, redis: Redis):
         self.pg_conn = pg_conn
+        self.redis = redis
+        self.logger = CustomLogger(name=self.__class__.__name__)
 
     @backoff(exceptions=(psycopg2.InterfaceError, psycopg2.OperationalError))
     @db_reconnect()
-    def extract_selected_films(self, selected_films: tuple[Any]):
-        """Достаем выбранные фильмы со всеми данными"""
-        # cur = self.pg_conn.cursor()
+    # def extract_selected_films(self, selected_films: tuple[Any, ...]):
+    def extract_selected_films(self, selected_films: tuple[Any, ...]):
+        """Извлекаем из БД выбранные фильмы со всеми необходимыми данными"""
         cur = self.pg_conn.cursor()
         cur.execute(
             f"SELECT "
@@ -157,11 +177,13 @@ class ProtoUpdater:
 
     @staticmethod
     def prepare_data_to_load(data: list[Film]):
+        """Подготовка данных для загрузки в ElasticSearch"""
         pre_data = []
         for film in data:
             pre_data.append({"index": {"_index": "movies", "_id": film.fw_id}})
             pre_data.append(
                 {
+                    "id": film.fw_id,
                     "imdb_rating": film.rating,
                     "genres": film.genres,
                     "title": film.title,
@@ -177,18 +199,18 @@ class ProtoUpdater:
         payload_data = '\n'.join(json.dumps(data) for data in pre_data) + '\n'
         return payload_data
 
-    @staticmethod
-    @backoff(exceptions=(ConnectionError,))
-    def load_data_to_elasticsearch(payload_data):
+    @backoff(exceptions=(requests.ConnectionError,))
+    def load_data_to_elasticsearch(self, payload_data):
+        """Загрузка данных в ElasticSearch"""
         url = 'http://127.0.0.1:9200/_bulk?filter_path=items.*.error'
         headers = {'content-type': 'application/x-ndjson'}
         response = requests.post(url, data=payload_data, headers=headers)
         if not response.status_code == 200:
-            pprint(f'Errors found: {response.text}')
+            self.logger.info(f'Errors found: {response.text}')
 
-    def _check_if_film_already_updated(self, data):
+    def _check_if_film_already_updated(self, data) -> tuple[Any, ...]:
         """
-        Проверяет, если фильм уже был обновлен в текущем цикле обновления.
+        Проверяет, если фильм уже был обновлен в текущем цикле обновления, для избежания повторного обновления.
         Возвращает кортеж требующих обновления фильмов
         """
         selected_films = []
@@ -200,29 +222,31 @@ class ProtoUpdater:
 
 
 class UpdateByPerson(ProtoUpdater):
+    """Класс для проверки и обновления данных по персоналиям"""
     needs_to_update: bool = True
     limit_rows = 100  # TODO Debug
-    # pg_conn: _connection
 
     @backoff(exceptions=(psycopg2.InterfaceError, psycopg2.OperationalError))
     @db_reconnect()
-    def extract_persons_data(self) -> tuple[str] | None:
-        """Достаем всех person у которых менялись данные (по дате обновления)"""
+    def extract_persons_data(self) -> tuple[Any, ...] | None:
+        """
+        Извлекаем из БД всех персоналий, у которых изменились данные с момента последнего обновления
+        """
         cur = self.pg_conn.cursor()
+        persons_last_update = self.redis.get('persons_last_update').decode('utf-8')
         cur.execute(
             f"SELECT id, updated_at "
             f"FROM content.person "
-            f"WHERE updated_at > '{self.persons_last_update}' "
+            f"WHERE updated_at > '{persons_last_update}' "
             f"ORDER BY updated_at "
             f"LIMIT {self.limit_rows};"
         )
         persons = cur.fetchall()
         if not persons:
             self.needs_to_update = False
-            return
+            return None
         persons_id = tuple(person[0] for person in persons)
 
-        # достаем список фильмов в которых они снимались
         cur.execute(
             f"SELECT fw.id, fw.updated_at "
             f"FROM content.film_work fw "
@@ -233,26 +257,39 @@ class UpdateByPerson(ProtoUpdater):
         )
         data = cur.fetchall()
 
-        self.persons_last_update = persons[-1]['updated_at']
-        selected_films = self._check_if_film_already_updated(data)
+        self.redis.set('persons_last_update', str(persons[-1]['updated_at']))
+        # selected_films = self._check_if_film_already_updated(data)
+        # if not selected_films:
+        #     self.needs_to_update = False
+        #     return None
+        selected_films = []
+        for film in data:
+            selected_films.append(film[0])
         if not selected_films:
             self.needs_to_update = False
-            return
-        return selected_films
+            return None
+        return tuple(selected_films)
 
 
 class UpdateByGenre(ProtoUpdater):
+    """Класс для проверки и обновления данных по жанрам"""
     needs_to_update: bool = True
     limit_obj_per_fetch = 100
 
     @backoff(exceptions=(psycopg2.InterfaceError, psycopg2.OperationalError))
     @db_reconnect()
-    def extract_genres_data(self) -> tuple[str] | None:  # TODO rename
+    def extract_and_load_genres_data(self) -> None:
+        """
+        Извлекаем из БД все жанры, у которых изменились данные с момента последнего обновления.
+        Т.к. изменение жанров касается большого количества фильмов, важно извлекать объекты из БД частями,
+        и сразу же подгружать обновленные данные в ElasticSearch.
+        """
         cur = self.pg_conn.cursor()
+        genres_last_update = self.redis.get('genres_last_update').decode('utf-8')
         cur.execute(
             f"SELECT id, updated_at "
             f"FROM content.genre "
-            f"WHERE updated_at > '{self.genres_last_update}' "
+            f"WHERE updated_at > '{genres_last_update}' "
             f"ORDER BY updated_at "
         )
         genres = cur.fetchall()
@@ -261,7 +298,6 @@ class UpdateByGenre(ProtoUpdater):
             return
         genres_id = tuple(genre[0] for genre in genres)
 
-        # достаем фильмы с указанными жанрами
         cur.execute(
             f"SELECT fw.id, fw.updated_at "
             f"FROM content.film_work fw "
@@ -270,88 +306,109 @@ class UpdateByGenre(ProtoUpdater):
             f'GROUP BY fw.id '
             f"ORDER BY fw.updated_at "
         )
-        self.genres_last_update = genres[-1]['updated_at']
+        self.redis.set('genres_last_update', str(genres[-1]['updated_at']))
         while data := cur.fetchmany(self.limit_obj_per_fetch):
-            selected_films = self._check_if_film_already_updated(data)
+            # selected_films = self._check_if_film_already_updated(data)
+            # if not selected_films:
+            #     self.needs_to_update = False
+            #     return
+            selected_films = []
+            for film in data:
+                selected_films.append(film[0])
             if not selected_films:
                 self.needs_to_update = False
-                return
+                return None
+            selected_films = tuple(selected_films)  # TODO list comprehenshion
             films_data = self.extract_selected_films(selected_films)
             payload_data = self.prepare_data_to_load(films_data)
             self.load_data_to_elasticsearch(payload_data)
 
 
 class UpdateByFilm(ProtoUpdater):
+    """Класс для проверки и обновления данных по фильмам"""
     needs_to_update: bool = True
     limit_rows = 100
 
     @backoff(exceptions=(psycopg2.InterfaceError, psycopg2.OperationalError))
     @db_reconnect()
-    def extract_films_data(self) -> tuple[str] | None:  # TODO rename
+    def extract_films_data(self) -> tuple[Any, ...] | None:
+        """
+        Извлекаем из БД все фильмы, у которых изменились данные с момента последнего обновления
+        """
         cur = self.pg_conn.cursor()
+        films_last_update = self.redis.get('films_last_update').decode('utf-8')
         cur.execute(
             f"SELECT id, updated_at "
             f"FROM content.film_work "
-            f"WHERE updated_at > '{self.films_last_update}' "
+            f"WHERE updated_at > '{films_last_update}' "
             f"ORDER BY updated_at "
             f"LIMIT {self.limit_rows} "
         )
         films = cur.fetchall()
-        selected_films = self._check_if_film_already_updated(films)
+        # selected_films = self._check_if_film_already_updated(films)
+        # if not selected_films:
+        #     self.needs_to_update = False
+        #     return None
+        selected_films = []
+        for film in films:
+            selected_films.append(film[0])
         if not selected_films:
             self.needs_to_update = False
-            return
-        self.films_last_update = films[-1]['updated_at']
+            return None
+        selected_films = tuple(selected_films)
+        self.redis.set('films_last_update', str(films[-1]['updated_at']))
         return selected_films
 
 
+def start_update_by_person(pg_conn: _connection, redis: Redis):
+    """Запуск процесса обновления данных по персоналиям"""
+    update_by_person = UpdateByPerson(pg_conn, redis)
+    update_by_person.logger.info('Запускаем обновление по персоналиям')
+    while update_by_person.needs_to_update is True:
+        if selected_films := update_by_person.extract_persons_data():
+            film_data = update_by_person.extract_selected_films(selected_films)
+            payload_data = update_by_person.prepare_data_to_load(film_data)
+            update_by_person.load_data_to_elasticsearch(payload_data)
+        else:
+            update_by_person.logger.info('Завершено обновление по персоналиям')
+    return update_by_person.pg_conn
+
+
+def start_update_by_genre(pg_conn: _connection, redis: Redis):
+    """Запуск процесса обновления данных по жанрам"""
+    update_by_genre = UpdateByGenre(pg_conn, redis)
+    update_by_genre.logger.info('Запускаем обновление по жанрам')
+    while update_by_genre.needs_to_update is True:
+        update_by_genre.extract_and_load_genres_data()
+    update_by_genre.logger.info('Завершено обновление по жанрам')
+    return update_by_genre.pg_conn
+
+
+def start_update_by_film(pg_conn: _connection, redis: Redis):
+    """Запуск процесса обновления данных по фильмам"""
+    update_by_film = UpdateByFilm(pg_conn, redis)
+    update_by_film.logger.info('Запускаем обновление по фильмам')
+    while update_by_film.needs_to_update is True:
+        if selected_films := update_by_film.extract_films_data():
+            film_data = update_by_film.extract_selected_films(selected_films)
+            payload_data = update_by_film.prepare_data_to_load(film_data)
+            update_by_film.load_data_to_elasticsearch(payload_data)
+        else:
+            update_by_film.logger.info('Завершено обновление по фильмам')
+    update_by_film.logger.info(
+        f'Обновление завершено. Следующее обновление начнется через {update_period} сек')
+    update_by_film.already_updated_films.clear()
+
+
 def main():
-    dsl = {
-        'dbname': os.environ.get('POSTGRES_DB'),
-        'user': os.environ.get('POSTGRES_USER'),
-        'password': os.environ.get('POSTGRES_PASSWORD'),
-        'host': os.environ.get('DB_HOST'),
-        'port': os.environ.get('DB_PORT'),
-    }
-    with closing(psycopg2.connect(**dsl, cursor_factory=DictCursor)) as pg_conn:
-        update_by_person = UpdateByPerson(pg_conn)
-        update_by_genre = UpdateByGenre(pg_conn) # TODO что если предыдущий конн уже закрылся, а актуальный коннект лежит в протоклассе. Можно ли хранить актуальный коннект в редис
-        # update_by_film = UpdateByFilm(pg_conn)
+    redis = Redis(**redis_connection)
+    while True:
+        with closing(psycopg2.connect(**dsl, cursor_factory=DictCursor)) as pg_conn:
+            pg_conn = start_update_by_person(pg_conn, redis)
+            pg_conn = start_update_by_genre(pg_conn, redis)
+            start_update_by_film(pg_conn, redis)
+        time.sleep(update_period)
 
-        print('запускаем апдейт персоналий')
-        while update_by_person.needs_to_update is True:
-            if selected_films := update_by_person.extract_persons_data():
-                film_data = update_by_person.extract_selected_films(selected_films)
-                payload_data = update_by_person.prepare_data_to_load(film_data)
-                update_by_person.load_data_to_elasticsearch(payload_data)
-            else:
-                print('log: no persons to update')
-
-        print('запускаем апдейт жанров')
-        # check_if_db_was_reconnected()
-        while update_by_genre.needs_to_update is True:
-            update_by_genre.extract_genres_data()
-        print('log: no genres to update')
-
-        # print('запускаем апдейт фильмов')
-        # while update_by_film.needs_to_update is True:
-        #     if selected_films := update_by_film.extract_films_data():
-        #         film_data = update_by_film.extract_selected_films(selected_films)
-        #
-        #         payload_data = update_by_film.prepare_data_to_load(film_data)
-        #         update_by_film.load_data_to_elasticsearch(payload_data)
-        #     else:
-        #         print('log: no films to update')
-        # print('all objects were updated. Next update start in ...')
-        #
-        # update_by_film.already_updated_films.clear()
-
-# def check_if_db_was_reconnected(prev_updater, next_updater) -> bool:
-#     if update_by_genre.pg_conn != update_by_person.pg_conn:  # если разорвался коннект в предыдущем апдейте, и был создан новый.
-#         update_by_genre.pg_conn = update_by_person.pg_conn  # то в следующем апдейте используем созданный коннект
-#
 
 if __name__ == '__main__':
     main()
-
-# TODO вывести код из мейн в отдельные функкции
